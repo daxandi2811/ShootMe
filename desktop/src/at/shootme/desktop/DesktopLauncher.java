64fde65d4bfcf5f00fa395965a3b47b3fa142d9a
Alexander Dietrich <diealb13@htl-kaindorf.ac.at>
2017-04-21 09:19:17 +0200

    removed resizability set icons in 16px, 32px, 128px

--- desktop/src/at/shootme/desktop/DesktopLauncher.java
+++ desktop/src/at/shootme/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package at.shootme.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import at.shootme.ShootMeGame;
@@ -10,6 +11,9 @@ public class DesktopLauncher {
 		config.title = "ShootMe";
 		config.width = 1280;
 		config.height = 720;
+		config.addIcon("assets/guenter_icon32px.png", Files.FileType.Internal);
+        config.addIcon("assets/guenter_icon16px.png", Files.FileType.Internal);
+        config.resizable = false;
 		new LwjglApplication(new ShootMeGame(), config);
 	}
 }
