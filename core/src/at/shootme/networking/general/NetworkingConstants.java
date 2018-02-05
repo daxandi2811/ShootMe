ad17749711080295641328c111d8504e42d69b7f
Cleatuer <nicole.wresnik@gmail.com>
2018-02-05 23:44:05 +0100

    [networking] change ports

--- core/src/at/shootme/networking/general/NetworkingConstants.java
+++ core/src/at/shootme/networking/general/NetworkingConstants.java
@@ -8,8 +8,8 @@ public class NetworkingConstants {
     public static final int WRITE_BUFFER_SIZE = 10000;
     public static final int OBJECT_BUFFER_SIZE = 10000;
 
-    public static final int TCP_PORT = 50000;
-    public static final int UDP_PORT = 50001;
+    public static final int TCP_PORT = 5000;
+    public static final int UDP_PORT = 5001;
 
     private NetworkingConstants() {
     }
